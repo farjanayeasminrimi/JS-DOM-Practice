dec02cd18eba0e9a9c3ac63b2c0c0429d09f196e
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-15 23:47:43 +0600

    added script file

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,7 @@
-console.log("hello world from external file");
+// console.log("hello world from external file");
+
+// get all classes
+const items = document.getElementsByClassName("list-item");
+for (let item of items) {
+  item.style.backgroundColor = "green";
+}
