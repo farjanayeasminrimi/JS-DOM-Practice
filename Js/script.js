63ee056220f6cc8e36a4859f7cb046db761a0ef9
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-16 21:15:09 +0600

    practiced DOM

--- Js/script.js
+++ Js/script.js
@@ -3,5 +3,25 @@
 // get all classes
 const items = document.getElementsByClassName("list-item");
 for (let item of items) {
-  item.style.backgroundColor = "green";
+  // item.style.backgroundColor = "green";
 }
+
+const title = document.querySelector("#title");
+const listAll = document.querySelectorAll("#menu .list-item a");
+for (let list of listAll) {
+  list.style.color = "Green";
+}
+
+// add classname
+
+const paragraph = document.querySelector("#paragraph");
+paragraph.classList.add("alignCenter");
+
+// remove classname
+paragraph.classList.remove("bg-green");
+
+// getAttribute & setAttribute
+const ancor = document.querySelector(".youtube");
+const link = ancor.getAttribute("href");
+const github = ancor.setAttribute("href", "https://github.com/farjanayeasminrimi/JS-DOM-Practice");
+console.log(ancor);
